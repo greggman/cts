bbd65361d58b546d54c4fc7742e55bbdf477c6a1
greggman <github@greggman.com>
2024-01-17 19:02:33 +0000

    Deploying to gh-pages from @ greggman/cts@9874613d69894ede3e7e4e0f7e409be8b2ef8366 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '1cc4ab0669fa254baf715cd274649249d37f2e5a';
+export const version = '9874613d69894ede3e7e4e0f7e409be8b2ef8366';
